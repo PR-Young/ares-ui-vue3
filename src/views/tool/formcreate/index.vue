f3cb5d8f999f38ac8b64100c56ab2bf298e3eb82
Young <1065569578@qq.com>
2023-12-13 09:26:30 +0800

    20231213|优化代码

--- src/views/tool/formcreate/index.vue
+++ src/views/tool/formcreate/index.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="container">
-    <ai-form-designer :show-header="false" style="width: 100%" />
+    <ai-form-designer
+      :show-header="true"
+      v-model="formData"
+      style="width: 100%"
+    />
   </div>
 </template>
+
 <script>
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import zhCn from "element-plus/es/locale/lang/zh-cn";
+export default {
+  data() {
+    return { formData: null };
+  },
+  watch: {
+    formData() {
+      console.log(this.formData);
+    },
+  },
+};
 </script>
