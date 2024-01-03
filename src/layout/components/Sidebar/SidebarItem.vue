73c81b80d8ad9be0bb8b892d0698f06624175570
Young <1065569578@qq.com>
2024-01-03 15:18:41 +0800

    20240103|js转为ts，版本4.0.0

--- src/layout/components/Sidebar/SidebarItem.vue
+++ src/layout/components/Sidebar/SidebarItem.vue
@@ -12,10 +12,16 @@
           :index="resolvePath(onlyOneChild.path)"
           :class="{ 'submenu-title-noDropdown': !isNest }"
         >
-          <item
-            :icon="onlyOneChild.meta.icon || (item.meta && item.meta.icon)"
-            :title="onlyOneChild.meta.title"
+          <svg-icon
+            :icon-class="
+              onlyOneChild.meta.icon || (item.meta && item.meta.icon)
+            "
           />
+          <template #title>
+            <span class="menu-title" :title="hasTitle(onlyOneChild.meta.title)"
+              >{{ onlyOneChild.meta.title }}
+            </span>
+          </template>
         </el-menu-item>
       </app-link>
     </template>
@@ -26,12 +32,11 @@
       :index="resolvePath(item.path)"
       teleported
     >
-      <template v-slot:title>
-        <item
-          v-if="item.meta"
-          :icon="item.meta && item.meta.icon"
-          :title="item.meta.title"
-        />
+      <template v-if="item.meta" #title>
+        <svg-icon :icon-class="item.meta && item.meta.icon" />
+        <span class="menu-title" :title="hasTitle(item.meta.title)">{{
+          item.meta.title
+        }}</span>
       </template>
       <sidebar-item
         v-for="child in item.children"
@@ -46,15 +51,14 @@
 </template>
 
 <script>
-
-import { isExternal } from '@/utils/validate'
-import Item from './Item.vue'
-import AppLink from './Link.vue'
-import FixiOSBug from './FixiOSBug.js'
-import path from 'path-browserify'
+import { isExternal } from "@/utils/validate";
+import Item from "./Item.vue";
+import AppLink from "./Link.vue";
+import FixiOSBug from "./FixiOSBug.js";
+import path from "path-browserify";
 
 export default {
-  name: 'SidebarItem',
+  name: "SidebarItem",
   components: { Item, AppLink },
   mixins: [FixiOSBug],
   props: {
@@ -69,47 +73,54 @@ export default {
     },
     basePath: {
       type: String,
-      default: '',
+      default: "",
     },
   },
   data() {
-    this.onlyOneChild = null
-    return {}
+    this.onlyOneChild = null;
+    return {};
   },
   methods: {
     hasOneShowingChild(children = [], parent) {
       const showingChildren = children.filter((item) => {
         if (item.hidden) {
-          return false
+          return false;
         } else {
           // Temp set(will be used if only has one showing child)
-          this.onlyOneChild = item
-          return true
+          this.onlyOneChild = item;
+          return true;
         }
-      })
+      });
 
       // When there is only one child router, the child router is displayed by default
       if (showingChildren.length === 1) {
-        return true
+        return true;
       }
 
       // Show parent if there are no child router to display
       if (showingChildren.length === 0) {
-        this.onlyOneChild = { ...parent, path: '', noShowingChildren: true }
-        return true
+        this.onlyOneChild = { ...parent, path: "", noShowingChildren: true };
+        return true;
       }
 
-      return false
+      return false;
     },
     resolvePath(routePath) {
       if (isExternal(routePath)) {
-        return routePath
+        return routePath;
       }
       if (isExternal(this.basePath)) {
-        return this.basePath
+        return this.basePath;
       }
-      return path.resolve(this.basePath, routePath)
+      return path.resolve(this.basePath, routePath);
+    },
+    hasTitle(title) {
+      if (title.length > 5) {
+        return title;
+      } else {
+        return "";
+      }
     },
   },
-}
+};
 </script>
